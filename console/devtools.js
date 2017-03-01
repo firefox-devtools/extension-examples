88828f998322a4155beaedd33b8cb42fa77280b3
Jan Odvarko <odvarko@gmail.com>
2017-03-01 08:58:27 +0100

    Tweak CSS + comments

--- console/devtools.js
+++ console/devtools.js
@@ -1,8 +1,8 @@
 /* See license.txt for terms of usage */
 
-var verbose = false;
-
-// Create new DevTools panel.
+/**
+ * Create new DevTools panel.
+ */
 chrome.devtools.panels.create(
   "My Console",
   "icon.png",
@@ -10,8 +10,11 @@ chrome.devtools.panels.create(
   initialize
 );
 
+/**
+ * Panel initialization
+ */
 function initialize(panel) {
   panel.onShown.addListener(function (win) {
-    // TODO
+    // TODO: initialization steps
   });
 }
